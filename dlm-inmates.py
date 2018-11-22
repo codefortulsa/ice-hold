6e1a30d5cefdde0597234fcbc25db9aefc162c75
jdungan <johnadungan@gmail.com>
2018-11-22 10:03:17 -0600

    request details for each inmate

--- dlm-inmates.py
+++ dlm-inmates.py
@@ -1,14 +1,16 @@
-import requests
+import re
 import csv
+import requests
 from urllib.parse import urlparse
 
 from bs4 import BeautifulSoup
 
 from settings import BOOKINGS_URL
-from _helpers import text_values
+from _helpers import text_values, clean_string
 
 
 iic_session = requests.Session()
+
 
 def param_generator():
     pages = range(1, 200)
@@ -19,19 +21,59 @@ def param_generator():
 def get_details_url(el):
     details_href = el.find('b').find('a')['href']
     parsed_url = urlparse(details_href)
-    k,v = parsed_url.query.split('=')
-    # separate and return the url and params
-    return (f'{BOOKINGS_URL}{parsed_url.path}',{k:v})
+    key, val = parsed_url.query.split('=')
+    return (f'{BOOKINGS_URL}{parsed_url.path}', {key: val})
 
 
 def get_inmate_details(inmate_row):
+    # this function collects header and value information
+    # in lists call 'detail_header' and 'detail_values'
+    # there are records with no details so a default header and
+    # no values are returned
+    detail_header = []
+    detail_values = []
+
+    # get the detail page
     details_url, details_param = get_details_url(inmate_row)
     response = iic_session.get(details_url, params=details_param)
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(details_url)
-    print(soup)
-    import ipdb; ipdb.set_trace()
-    return
+
+    arrest_start = soup.find('h4',string=re.compile(r'Arrest Information'))
+
+    if arrest_start:
+        arrest_thead = arrest_start.find_next('div','details').find_next('thead')
+        arrest_header = text_values(arrest_thead.find_all('th'))
+        detail_header = detail_header + arrest_header
+
+        detail_row = arrest_thead.find_next('tbody').find('tr')
+        row_values = text_values(detail_row.find_all('td'))
+        detail_values = detail_values + row_values
+
+        # This section will find string after a {key_word} and
+        # add the values after ':'
+        # key words available but not used: Height, Weight, Hair, Eyes
+        key_words = ['Race']
+        for word in key_words:
+            word_element = soup.find(string=re.compile(word))
+            if word_element:
+                detail_header.append(word)
+                word_value =word_element.split(':')[1]
+                detail_values.append(clean_string(word_value))
+
+        # look for a TLS record
+        tls_element = soup.find(string=re.compile(r'TLS\d*'))
+        if tls_element:
+            detail_values.append(clean_string(tls_element))
+        else:
+            detail_values.append('')
+        # add TLS even if not found
+        detail_header.append('TLS')
+    else:
+        # this could be wrong, but will only matters if the first inmate
+        # on the first page has no arrrest details
+        detail_header = ['Arrest Date', 'Arrest Time', 'Arrested By', 'Booking Date', 'Booking Time', 'Release Date', 'Release Time', 'Race', 'TLS']
+
+    return (detail_header, detail_values)
 
 
 def get_inmate_list(params):
@@ -40,12 +82,15 @@ def get_inmate_list(params):
     inmate_table = soup.find('table', 'table table-striped grid-table')
     thead = inmate_table.find('thead').find_all('th')
     rows = inmate_table.find('tbody').find_all('tr')
-    for row in rows:
-        details = get_inmate_details(row)
 
+    inmate_rows = []
+    for row in rows:
+        (detail_header, detail_values) = get_inmate_details(row)
+        inmate_rows.append(text_values(row.find_all('td')) + detail_values)
+    headers = text_values(thead) + detail_header
     # return a list of column headers and a list of table rows
     # each row is a list of the values in the row
-    return (text_values(thead), [text_values(row.find_all('td')) for row in rows])
+    return (headers, inmate_rows)
 
 # open a file for writing
 
@@ -60,7 +105,7 @@ with open('data/dlm_inmates.csv', 'w', newline='') as csvfile:
 
     while True:
         last_inmates = inmates
-        (header, inmates) = get_values(next(request_params))
+        (header, inmates) = get_inmate_list(next(request_params))
         if inmates == last_inmates:
             break
 
