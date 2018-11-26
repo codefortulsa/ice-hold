a6b4c4b511635348a05d93d3a1cb195107d9e5a7
jdungan <johnadungan@gmail.com>
2018-11-25 19:18:18 -0600

    get hold information, update README

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 The [Tulsa County Inmate Information Center](http://iic.tulsacounty.org/expInmateBookings/BookingIndex) website provides a page to scroll through a list of inmates.  The dlm-inmates script retrieves all pages and also looks up inmate details.  Inmates with an ICE hold are denoted by the 'TLS' number in their record.
 
+The City Of Tulsa Jail Hub has information about inmates at the [Tulsa City Jail](https://www.cityoftulsa.org/apps/inmateinformationcenter/) for the past 90 days.
+
+The CSV files created by these scripts includes a column to identify inmates on an ICE hold.  The dlm-inmates file uses **'TLS'** and includes a TLS record number.  The tulsa_city_inmates file uses **'hold'** and shows the last hold instructions on the inmates charges list.
+
 ## Files
 * __dlm-inmates.py__: python script for saving iic.tulsacounty.org info to a csv
+* __tulsa-jail.py__: python script for saving www.cityoftulsa.org information
 * __settings.py__: details of the tulsacounty url
 * __\_helpers.py__: utilities for cleaning text
 
@@ -16,5 +21,10 @@ The [Tulsa County Inmate Information Center](http://iic.tulsacounty.org/expInmat
 
 ## Usage
 
-1. 'python dlm-inmates.py'
-1. creates a csv file in data/ called dlm_inmates.csv
+### David L Moss Inmates:
+* 'python dlm-inmates.py'
+* creates a csv file in data/ called dlm_inmates.csv
+
+### City of Tulsa Jail:
+* 'python tulsa-jail.py'
+* creates a csv file in data/ called tulsa_city_inmates.csv
--- tulsa-jail.py
+++ tulsa-jail.py
@@ -21,35 +21,36 @@ with requests.Session() as iic_session:
 
     def get_hold_information(details_param):
         hold_info_from = lambda item: True if item['hold'] else False
-        # docket info which includes hold element
-        docket = get_aspx_payload('Incident', params=details_param)
-        for i in docket:
-            print(i['hold'])
-        holds = filter(hold_info_from, docket)
-        hold_values = [h for h in holds]
+
+        # the chages list includes a hold element
+        charges = get_aspx_payload('Incident', params=details_param)
+        charges_with_holds = filter(hold_info_from, charges)
+        hold_instructions = list(map(lambda c: c['hold'], charges_with_holds))
+
         # return last hold othewise empty str
-        return hold_values[len(hold_values)-1] if hold_values else ''
+        # The '[::-1][0]' reverse sorts the list and returns the first item
+        return hold_instructions[::-1][0] if len(hold_instructions) > 0 else ''
 
-
-    def get_inmate_details(details_param):
+    def get_inmate_details(params):
         # this function collects header and value information
         # in lists call 'detail_header' and 'detail_values'
 
         # get Inmate details
-        inmates = get_aspx_payload('InmateInfo', params=details_param)
-        detail_header = [k for k in inmates.keys()]
-        detail_values = dict_values(inmates)
+        inmate = get_aspx_payload('InmateInfo', params=params)
+        detail_header = [k for k in inmate.keys()]
+        detail_values = dict_values(inmate)
 
         # look up hold information
         detail_header.append('hold')
-        detail_values.append(get_hold_information(details_param))
+        hold_text = get_hold_information(params)
+        detail_values.append(hold_text)
 
         return(detail_header, detail_values)
 
     def get_inmate_list():
         print('POSTing request for all inmates in the past 90 days')
 
-        incident_id = lambda inmate: {'dataId': inmate['IncidentRecordID']}
+        id_params = lambda inmate: {'dataId': inmate['IncidentRecordID']}
 
         all_inmates_list = get_aspx_payload('CompleteInmates', params='')
 
@@ -59,24 +60,22 @@ with requests.Session() as iic_session:
         first_inmate_values = dict_values(first_inmate)
 
         # get details header from first inmate
-        (first_inmate_header, first_inmate_details) = (
-            get_inmate_details(incident_id(first_inmate))
+        (first_details_header, first_inmate_details) = (
+            get_inmate_details(id_params(first_inmate))
             )
-
         headers = (
-            [k for k in first_inmate.keys()] + first_inmate_header
+            [k for k in first_inmate.keys()] + first_details_header
             )
-        #  start a list of row with the first inmate values
-        inmate_rows =[]
+        #  start a list of rows with the first inmate values
+        inmate_rows = []
         inmate_rows.append(first_inmate_values + first_inmate_details)
 
         # get the details on the remaining inmates
         for inmate in inmates:
             (detail_header, detail_values) = (
-                    get_inmate_details(incident_id(first_inmate))
+                get_inmate_details(id_params(inmate))
                 )
             inmate_rows.append(dict_values(inmate) + detail_values)
-
 
         # return a list of column headers and a list of table rows
         return (headers, inmate_rows)
